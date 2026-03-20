87726a0f6849b9a9bc839582a1966784d28c1a38
Kris Baumgartner <kjbaumgartner@gmail.com>
2026-03-19 17:53:23 -0700

    🔧 Fix the build

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,8 @@
-#!/usr/bin/env tsx
+#!/usr/bin/env node
 import { runCLI } from './runner.ts';
 
-await runCLI(process.argv.slice(2));
+async function main(): Promise<void> {
+  await runCLI(process.argv.slice(2));
+}
+
+void main();
